42764e85269be2ec5b44e79d269978464c686d58
Tom Brookes <tom.brookes15@gmail.com>
2022-05-28 16:32:31 +0100

    validations for items and favourite_list

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :list
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :list_id, presence: true
+  validates :description, length: { maximum: 200 }
 end
