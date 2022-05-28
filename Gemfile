bb7c268ca26ade76fd60492828ff56cf73d84243
Nuria Toledano <ia.andyouare@gmail.com>
2022-05-28 15:45:16 +0100

    added validations to user and follow'

--- Gemfile
+++ Gemfile
@@ -65,7 +65,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # added from Cloudinary slides
-gem 'dotenv-rails', groups: [:development, :test]
-
 gem 'cloudinary', '~> 1.16.0'
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
+
+  # Will return an array of follows for the given user instance
+  has_many :received_follows, foreign_key: :followed_id, class_name: "Follow"
+  # Will return an array of users who follow the user instance
+  has_many :followers, through: :received_follows, source: :follower
+
+  # returns an array of follows a user gave to someone else
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+  # returns an array of other users who the user has followed
+  has_many :followings, through: :given_follows, source: :followed
+
+  # validates :username, uniqueness: { case_sensitive: true }
+  # validates :username, presence: true
+  # validates :bio, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
