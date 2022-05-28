f9f4424647e5c5efa7965aaadd181b333f5e34ef
Tom Brookes <tom.brookes15@gmail.com>
2022-05-28 14:38:39 +0100

    working on the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning Database..."
+User.destroy_all
+Following.destroy_all
+List.destroy_all
+Item.destroy_all
+FavouriteList.destroy_all
+Tag.destroy_all
+ListTag.destroy_all
+puts "Database clean"
+
+puts "Creating Users..."
+user1 = User.create()
+puts "Creating Following..."
+
+puts "Creating Lists..."
+
+puts "Creating List Items..."
+
+puts "Creating Favourite Lists..."
+
+puts "Creating Tags..."
+
+puts "Creating List Tags..."
