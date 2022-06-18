dc979dc553b2efe22f57ee7338ed1da36f36d471
Ana Arias <athelas85@gmail.com>
2022-06-18 16:24:43 +0100

    all the checklist should work now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: :configure_permitted_parameters
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  end
-
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
 end
