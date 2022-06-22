8e406830d2e85c921f61367016c6930d5b8f3bfc
Tom Brookes <tom.brookes15@gmail.com>
2022-06-22 23:37:17 +0100

    hide text in article view for users

--- app/javascript/controllers/edit_item_controller.js
+++ app/javascript/controllers/edit_item_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["infos", "form", "card", "editor", "article"];
 
+  connect() {
+    this.articleTarget.classList.add("d-none")
+  }
+
   displayForm() {
     this.infosTarget.classList.add("d-none")
     this.formTarget.classList.remove("d-none")
@@ -13,7 +17,14 @@ export default class extends Controller {
 
   descBtn() {
     this.articleTarget.classList.toggle("d-none")
+    console.log("clicked")
   }
+
+  //displayArticle() {
+    // this.artViewTarget.nextElementSibling.classList.toggle("d-none")
+    //this.articleTarget.classList.toggle("d-none")
+    //console.log("clicked")
+  //}
 
   update(event) {
     event.preventDefault()
