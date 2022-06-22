cacaef278756bbcb0ce552e8d8d1220898eff24d
Ana AG <51890080+Athelas85@users.noreply.github.com>
2022-06-22 21:03:41 +0100

    Update social_share_button.rb

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook tumblr email linkedin reddit telegram whatsapp_app)
+  config.allow_sites = %w(twitter facebook tumblr reddit telegram whatsapp_app)
 end
