54808db384d3f8caf29d40e430e0f5afe2fd19f1
Nuria Toledano <ia.andyouare@gmail.com>
2022-06-23 19:20:05 +0100

    Devise signup and login frontend changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @lists = List.followings_lists(current_user.followings.ids)
+    @lists = List.followings_lists(current_user.followings.ids) if current_user
   end
 
 end
