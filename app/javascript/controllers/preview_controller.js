a4d1eda7596402a41f950fecf59879c3b360262f
Tom Brookes <tom.brookes15@gmail.com>
2022-06-24 18:33:26 +0100

    preview appears to be working, need to sort out the logic of the editor/preview button up top

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -1,24 +1,40 @@
-const preview = document.getElementById("preview")
-const buttons = document.getElementById("buttons")
+const preview = document.querySelector("#preview")
+
+const title = document.querySelector("#title")
+const tags = document.querySelector("#tags")
+const add = document.querySelector("#add")
+const article = document.querySelector("#art")
+const buttons = document.querySelector("#buttons")
 const listEdits = document.querySelectorAll(".list-editor")
 
 function pT() {
+    preview.classList.toggle("selected");
+    title.classList.toggle("d-none");
+    tags.classList.toggle("d-none");
+    add.classList.toggle("d-none");
+    article.classList.toggle("d-none");
     buttons.classList.toggle("d-none");
     listEdits.forEach((edit) => {
       edit.classList.toggle("d-none")
     });
 }
 
-import { Controller } from "stimulus"
+preview.addEventListener("click", pT)
 
-export default class extends Controller {
-  static targets = ["title", "tags", "add", "article"];
-
-  previewToggle() {
-    this.titleTarget.classList.toggle("d-none")
-    this.tagsTarget.classList.toggle("d-none")
-    this.addTarget.classList.toggle("d-none")
-    this.articleTarget.classList.toggle("d-none")
-    pT()
-  }
+if (preview.innerHTML == "Editor") {
+  pT()
 }
+// import { Controller } from "stimulus"
+
+// export default class extends Controller {
+//   static targets = ["title", "tags", "add", "article"];
+
+//   previewToggle() {
+//     this.titleTarget.classList.toggle("d-none")
+//     this.tagsTarget.classList.toggle("d-none")
+//     this.addTarget.classList.toggle("d-none")
+//     this.articleTarget.classList.toggle("d-none")
+//     pT()
+//   }
+
+// }
